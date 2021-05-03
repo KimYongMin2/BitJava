6e54016318200ea5329ea8f14e2994f82ea80f2a
KimYongMin2 <kym960304@naver.com>
2021-05-03 20:26:19 +0900

    새출발

---
+++ workspace/src/day1_1/HelloJavaPrograming.java
@@ -0,0 +1,7 @@
+package day1_1;
+
+public class HelloJavaPrograming {
+    public static void main(String args[]) {
+        System.out.println("안녕하세요.자바^^*");
+    }
+}
---
+++ workspace/src/day1_1/Sample03.java
@@ -0,0 +1,24 @@
+package day1_1;
+
+class MyJavaWork{
+    String myName;
+}
+public class Sample03 {
+
+    public static void main(String[] args) {
+        int a = 1000;
+        float a2 = 10.2f;
+        String name = "안녕하세요.";
+        char b = 't';
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(b);
+//		"안녕하세요."
+//		20
+//		20.2
+//		false
+
+
+    }
+
+}
