7831f8b08a41bc8c232f6f06d9d21355f4d45dc6
KimYongMin2 <kym960304@naver.com>
2021-05-06 19:57:47 +0900

    단순화 및 잘한학생 추가

--- workspace/src/test/arrayTest/Array.java
+++ workspace/src/test/arrayTest/Array.java
@@ -4,30 +4,65 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        char[][] studentAnswer = new char[5][10];
-        char [] answer = new char[10];
-        int correct;
+        char[][] studentAnswer = new char[3][3];
+        char [] answer = new char[3];
+        char[][] studentAnswerView = new char[3][3];
+        int[] studentScore = new int[3];
         getStudentAnswer(studentAnswer);
         getAnswer(answer);
         showStudentAnswer(studentAnswer);
         showAnswer(answer);
-        showTotalScore(studentAnswer, answer);
-
+        getStudentAnswerView(studentAnswer, answer, studentAnswerView, studentScore);
+        showStudentAnswerView(studentAnswerView);
+        showTotalScore(studentScore);
+        showGreatStudent(studentScore);
+    }
+    private static void showGreatStudent(int[] studentScore) {
+        System.out.println("------------------------------------------------");
+        int great=0;
+        for (int i = 0; i < studentScore.length; i++) {
+            if (studentScore[i] > studentScore[great]){
+                great = i;
+            }
+        }
+        System.out.println("제일 잘한 학생은 : 학생" + (great+1));
     }
 
-    private static void showTotalScore(char[][] studentAnswer, char[] answer) {
-        int correct;
+    private static void showStudentAnswerView(char[][] studentAnswerView) {
         System.out.println("------------------------------------------------");
+        for (int i = 0; i < studentAnswerView.length; i++) {
+            System.out.print("학생 " + (i+1) + "의 채점결과 :");
+            for (int j = 0; j < studentAnswerView[i].length; j++) {
+                System.out.print(studentAnswerView[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void getStudentAnswerView(char[][] studentAnswer, char[] answer, char[][] studentAnswerView, int[] studentScore) {
+        int correct;
         for (int i = 0; i < studentAnswer.length; i++) {
             correct = 0;
             for (int j = 0; j < studentAnswer[i].length; j++) {
-                if(studentAnswer[i][j] == answer[j]){
+                if (studentAnswer[i][j] == answer[j]) {
+                    studentAnswerView[i][j] = 'O';
                     correct ++;
+                } else {
+                    studentAnswerView[i][j] = 'X';
                 }
+                studentScore[i] = correct;
+
             }
-            System.out.println("The Student["+ (i+1) +"]'s Score is " + correct);
         }
     }
+
+    private static void showTotalScore(int[] studentScore) {
+        System.out.println("------------------------------------------------");
+        for (int i = 0; i < studentScore.length; i++) {
+            System.out.println("The Student["+ (i+1) +"]'s Score is " + studentScore[i]);
+        }
+    }
+    
 
     private static void showAnswer(char[] answer) {
         System.out.println("------------------------------------------------");
@@ -52,7 +87,7 @@ public class Array {
     private static void getAnswer(char[] answer) {
         System.out.println("정답을 입력하여 주세요");
         for (int i = 0; i < answer.length; i++) {
-            System.out.print(i+1 + "번의 답 : ");
+            System.out.print(i+1 + "번의 정답 : ");
             answer[i] = getUserInputChar();
         }
         System.out.println();
