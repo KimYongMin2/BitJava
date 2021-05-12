204a25904cea91df41c0fd82821b849cd0670c73
KimYongMin2 <kym960304@naver.com>
2021-05-12 18:10:59 +0900

    코드간편하게

--- Java2ws1/src/ws1/java2/controller/VehicleManager.java
+++ Java2ws1/src/ws1/java2/controller/VehicleManager.java
@@ -3,6 +3,7 @@ package ws1.java2.controller;
 import ws1.java2.entity.*;
 
 public class VehicleManager {
+    /*
     Airplane[] airplaneArr;
     Car[] carArr;
     Ship[] shipArr;
@@ -41,6 +42,30 @@ public class VehicleManager {
             shipArr[inx].setAvailable(true);
         }
 
+        System.out.println();
+    }
+}
+    */
+
+    Vehicle[] vehicle;
+
+    public VehicleManager() {
+        vehicle = new Vehicle[7];
+        vehicle[0] = new Airplane("보잉747", 1300, 300, 4);
+        vehicle[1] = new Airplane("F-15", 1600, 1, 1);
+        vehicle[2] = new Car("그랜저", 180, 5, 10);
+        vehicle[3] = new Car("스파크", 130, 4, 15);
+        vehicle[4] = new Car("스타렉스", 150, 10, 11);
+        vehicle[5] = new Ship("크루즈2", 30, 400, 35000);
+        vehicle[6] = new Ship("노틸러스", 25, 150, 15000);
+    }
+
+    public void displayVehicles(String title) {
+        System.out.println(title);
+        for (int i = 0; i < vehicle.length; i++){
+            vehicle[i].displayInfo();
+            vehicle[i].setAvailable(true);
+        }
         System.out.println();
     }
 }
