894ba3099b3106ccc5bfa0aa30d32c192e665baa
KimYongMin2 <kym960304@naver.com>
2021-05-14 15:06:12 +0900

    계좌 정보 확인시 null값 확인

--- Workshop03/src/com/test/workshop3/account/Account.java
+++ Workshop03/src/com/test/workshop3/account/Account.java
@@ -1,5 +1,6 @@
 package com.test.workshop3.account;
 
+import com.test.workshop3.exception.AccountNotFoundException;
 import com.test.workshop3.exception.InvalidValueException;
 
 public class Account {
@@ -11,7 +12,7 @@ public class Account {
     public Account(String account, String name, int balance) throws InvalidValueException {
         //TODO 최초 입금 금액이 0원 이하인 경우, 예외 발생
         if (balance <= 0) {
-            throw new InvalidValueException("최초금액은 0원 이하가 될 수 없습니다");
+            throw new InvalidValueException("[에러] 계좌 생성 시, 최초 입금 금액이 필요합니다");
         }
         this.account = account;
         this.name    = name;
@@ -20,19 +21,25 @@ public class Account {
 
     public void deposit(int money) throws InvalidValueException {
             //TODO 입금할 금액이 잘못 입력된 경우, 예외 발생
+        if (money < 0) {
+            throw new InvalidValueException("[에러] 입금할 금액은 0원 초과의 숫자로 입력하세요");
+        }
             balance += money;
     }
 
     public void withdraw(int money) throws InvalidValueException {
         //TODO 출금할 금액이 잘못 입력되었거나, 잔액이 부족한 경우 예외 발생
+        if (money < 0) {
+            throw new InvalidValueException("[에러] 출금할 금액은 0원 초과의 숫자로 입력하세요");
+        }
         balance -= money;
         if (balance < 0) {
-            throw new InvalidValueException("잔액부족");
+            balance += money;
+            throw new InvalidValueException("[에러] 잔액이 부족합니다");
         }
     }
 
-    @Override
     public String toString() {
-        return "[계좌] " + account + "\t[예금주] " + name + "\t[잔액] " + balance;
+            return "[계좌] " + account + "\t[예금주] " + name + "\t[잔액] " + balance;
     }
 }
--- Workshop03/src/com/test/workshop3/account/AccountTest.java
+++ Workshop03/src/com/test/workshop3/account/AccountTest.java
@@ -1,5 +1,6 @@
 package com.test.workshop3.account;
 
+import com.test.workshop3.exception.AccountNotFoundException;
 import com.test.workshop3.exception.InvalidValueException;
 
 import java.io.BufferedReader;
@@ -28,18 +29,20 @@ public class AccountTest {
 
                         break;
                     case 2:     // 계좌 정보 출력
+                        catchNull(acc);
                         System.out.println(acc);
                         break;
                     case 3:     // 입금
+                        catchNull(acc);
                         System.out.print("입금할 금액 : ");
-                        int upBalance = Integer.parseInt(getUserInput());
-                        acc.deposit(upBalance);
+                            int upBalance = Integer.parseInt(getUserInput());
+                            acc.deposit(upBalance);
                         break;
                     case 4:     // 출금
+                        catchNull(acc);
                         System.out.print("출금할 금액 : ");
-                        int downBalance = Integer.parseInt(getUserInput());
-                        acc.withdraw(downBalance);
-
+                            int downBalance = Integer.parseInt(getUserInput());
+                            acc.withdraw(downBalance);
                         break;
                     case 9:     // 종료
                         System.out.println("종료합니다.");
@@ -51,15 +54,18 @@ public class AccountTest {
 
                 //TODO catch 세분화 (각각의 예외 상황 별로)
             } catch (InvalidValueException e) {
-                System.out.println("[에러] 에러가 발생했습니다.");
                 System.out.println(e.getMessage());
-            } catch (NumberFormatException c) {
-                System.out.println("[에러] 에러가 발생했습니다.");
-                System.out.println("잘못된 값입니다");
-                System.out.println(c.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("숫자로 입력해야 합니다.");
+            } catch (AccountNotFoundException e) {
+                System.out.println(e.getMessage());
             }
 
         }
+    }
+
+    private static void catchNull(Account acc) throws AccountNotFoundException {
+        if (acc == null){ throw new AccountNotFoundException("[에러] 계좌가 없습니다"); }
     }
 
     private static void printMenu() {
