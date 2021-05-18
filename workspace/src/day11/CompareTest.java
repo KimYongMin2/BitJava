b5b3154e0bbc1f3019c673a3f6a977c14ea8afca
KimYongMin2 <kym960304@naver.com>
2021-05-18 15:32:41 +0900

    크기비교 대소문자까지

---
+++ workspace/src/day11/CompareTest.java
@@ -0,0 +1,12 @@
+package day11;
+
+public class CompareTest {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        System.out.println(a.compareTo(b));
+        System.out.println(a.compareTo(a));
+        System.out.println(b.compareTo(b));
+        System.out.println(b.compareTo(a));
+    }
+}
